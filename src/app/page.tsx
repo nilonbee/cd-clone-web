1f5d2637124644d656d01a91b51b3e51ec73d79b
nilonbee <nilonbee@gmail.com>
2024-03-07 21:32:30 +0530

    configured IPINFO to the web 2.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,50 @@
+// "use client";
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+// import React from "react";
+
+// export default async function Home() {
+//   const router = useRouter();
+//   const request = await fetch("https://ipinfo.io/json?token=ef23007cda81cb");
+//   const jsonResponse = await request.json();
+
+//   console.log(jsonResponse.ip, jsonResponse.country);
+
+//   useEffect(() => {
+//     if (router) {
+//       router.push(`/${jsonResponse.country}`); // Redirect to Sri Lanka by default
+//     }
+//   }, [router]);
+
+//   return <></>;
+// }
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import React from "react";
+import { useEffect, useState } from "react";
+import fetchLocation from "@/features/ipInfo";
 
-export default function Home() {
+const Home = () => {
   const router = useRouter();
-
   useEffect(() => {
-    if (router) {
-      router.push("/lk"); // Redirect to Sri Lanka by default
-    }
+    const fetchData = async () => {
+      try {
+        const country = await fetchLocation({
+          url: `https://ipinfo.io/json?token=${process.env.NEXT_PUBLIC_IP_INFO_TOKEN}`,
+        });
+        console.log(country);
+        if (router) {
+          router.push(`/${country}`);
+        }
+      } catch (error) {
+        console.error("Error fetching location:", error);
+        // Handle errors appropriately
+      }
+    };
+
+    fetchData();
   }, [router]);
 
   return <></>;
-}
+};
+
+export default Home;
