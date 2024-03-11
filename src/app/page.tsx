2003bca16ef7958324504977b1026292bf1ae4fa
nilonbee <nilonbee@gmail.com>
2024-03-11 11:35:14 +0530

    resolving issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,25 @@
-// "use client";
-// import { useRouter } from "next/navigation";
-// import { useEffect } from "react";
-// import React from "react";
-
-// export default async function Home() {
-//   const router = useRouter();
-//   const request = await fetch("https://ipinfo.io/json?token=ef23007cda81cb");
-//   const jsonResponse = await request.json();
-
-//   console.log(jsonResponse.ip, jsonResponse.country);
-
-//   useEffect(() => {
-//     if (router) {
-//       router.push(`/${jsonResponse.country}`); // Redirect to Sri Lanka by default
-//     }
-//   }, [router]);
-
-//   return <></>;
-// }
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import fetchLocation from "@/features/ipInfo";
+import { useEffect } from "react";
+import fetchLocation from "@/utils/ipInfo";
 
 const Home = () => {
   const router = useRouter();
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const country = await fetchLocation({
           url: `https://ipinfo.io/json?token=${process.env.NEXT_PUBLIC_IP_INFO_TOKEN}`,
         });
-        console.log(country);
         if (router) {
           router.push(`/${country}`);
+        } else {
+          console.error("Router is not defined, redirecting to /LK");
+          router.push("/LK");
         }
       } catch (error) {
         console.error("Error fetching location:", error);
-        // Handle errors appropriately
       }
     };
 
