8ef97c063da5ce6008724066924fa0173b9a17cc
nilonbee <nilonbee@gmail.com>
2024-03-19 16:36:04 +0530

    style fix

--- src/components/molecules/PhoneInput/index.tsx
+++ src/components/molecules/PhoneInput/index.tsx
@@ -2,26 +2,27 @@
 import React from "react";
 import { PhoneInput } from "react-international-phone";
 import "react-international-phone/style.css";
+import "./phone.module.css";
 
 export const PhoneInputField: React.FC = () => {
   const [phone, setPhone] = React.useState("");
 
   return (
     <div>
-      <label className="block text-gray text-sm font-medium -mt-2">
+      <label className="block text-gray text-sm font-medium -mt-4">
         Phone Number<span className="text-red">*</span>
       </label>
       <PhoneInput
         defaultCountry="lk"
         value={phone}
-        inputStyle={{ height: "41px", border: "none", minWidth: "25px" }}
+        inputStyle={{ height: "41px", border: "none" }}
         countrySelectorStyleProps={{
           style: {
             boxShadow: "none",
             border: "none",
           },
         }}
-        className="border border-border first-letter:rounded text-gray leading-tight focus:outline-none focus:shadow-outline rounded h-[46px]"
+        className="border border-border first-letter:rounded text-gray leading-tight focus:outline-none focus:shadow-outline rounded h-[46px] mt-2"
       />
       <style>
         {`
