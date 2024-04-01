82fb16ffbdf28357841fff2e9b4a4481987e333b
nilonbee <nilonbee@gmail.com>
2024-04-01 22:58:09 +0530

    fix: fixed careers page and single career page integration

--- src/components/molecules/CareerRow/index.tsx
+++ src/components/molecules/CareerRow/index.tsx
@@ -1,30 +1,35 @@
-import { MainButton } from "@/components/atoms";
+import parse from "html-react-parser";
 import Link from "next/link";
-import React from "react";
 
-export const CareerRow = () => {
+import { MainButton } from "@/components/atoms";
+import { ICareer } from "@/types/careers";
+
+export const CareerRow = ({
+  title,
+  end_date,
+  created_at,
+  description,
+  slug,
+}: ICareer) => {
   return (
     <div className="border border-boxBorder p-8 flex justify-between items-center self-stretch relative w-full bg-white rounded shadow">
       <div className="relative bg-transparent w-2/5">
         <p className="font-semibold leading-[26px] text-base text-textColor">
-          Student Counselor
+          {title}
         </p>
         <p className="leading-[26px] text-sm text-textColor line-clamp-4">
-          Bachelors or above qualifications Minimum 2 -3 years of counselling
-          experience in the education industry (Not mandatory) Excellent
-          communication skills and convincing power with students Ability to
-          develop a strong relationship with overseas universities and instit..
+          {description ? parse(description) : ""}
         </p>
       </div>
       <div className="flex flex-col gap-2 items-start relative bg-transparent">
         <p className="leading-5 text-sm text-textColor">
-          Posted Date: 2023-06-30 04:49:40
+          Posted Date: {`${created_at}`}
         </p>
         <p className="leading-5 text-sm text-textColor">
-          End Date : 2023-11-30
+          End Date: {` ${end_date}`}
         </p>
       </div>
-      <Link href="/lk/careers/1">
+      <Link href={`/lk/careers/${slug}`}>
         <MainButton label="Apply Now" btnStyle="Secondary" btnSize="Large" />
       </Link>
     </div>
