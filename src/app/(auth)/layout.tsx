07d5a8c65eb2c65e5aaf9bbe30dfea0705a5f6e2
nilonbee <nilonbee@gmail.com>
2024-06-11 12:32:44 +0530

    fix: back button added

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,20 @@
+import { AiOutlineClose } from "react-icons/ai";
+import Link from "next/link";
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <section>{children}</section>;
+  return (
+    <section>
+      <Link
+        href="/"
+        className="absolute right-[48px] top-[48px] w-[24px] h-[24px]"
+      >
+        <AiOutlineClose className="text-xl font-semibold" />
+      </Link>
+      {children}
+    </section>
+  );
 }
