b24f12dd1223b94385e124e78c19e1337e82fb89
nilonbee <nilonbee@gmail.com>
2024-06-18 19:10:06 +0530

    fix: drawer

--- src/components/organisms/CourseViewDrawer/index.tsx
+++ src/components/organisms/CourseViewDrawer/index.tsx
@@ -9,37 +9,13 @@ import { CourseSubHeader, LoadingSpinner } from "@/components/atoms";
 import { ApplicationForm } from "../ApplicationForm";
 import { IIntake } from "@/types/intakes";
 import { AiOutlineClose } from "react-icons/ai";
+import { CustomDrawer } from "@/components/molecules/Drawer";
 
 interface CourseViewDrawerProps {
-  side?: "right" | "left" | "top" | "bottom";
   intakes: IIntake[];
 }
 
-const openClassNames = {
-  right: "translate-x-0",
-  left: "translate-x-0",
-  top: "translate-y-0",
-  bottom: "translate-y-0",
-};
-
-const closeClassNames = {
-  right: "translate-x-full",
-  left: "-translate-x-full",
-  top: "-translate-y-full",
-  bottom: "translate-y-full",
-};
-
-const classNames = {
-  right: "inset-y-0 right-0",
-  left: "inset-y-0 left-0",
-  top: "inset-x-0 top-0",
-  bottom: "inset-x-0 bottom-0",
-};
-
-export const CourseViewDrawer = ({
-  side = "bottom",
-  intakes,
-}: CourseViewDrawerProps) => {
+export const CourseViewDrawer = ({ intakes }: CourseViewDrawerProps) => {
   const { selectedCourseId, isEmpty, openDrawer, setOpenDrawer } =
     useCourseFilterStore();
   const [data, setData] = useState<ICourse>({} as ICourse);
@@ -59,130 +35,83 @@ export const CourseViewDrawer = ({
   }, [selectedCourseId, isEmpty]);
 
   return (
-    <div
-      id={`dialog-${side}`}
-      className="relative z-20"
-      aria-labelledby="slide-over"
-      role="dialog"
-      aria-modal="true"
-      onClick={() => setOpenDrawer(!openDrawer)}
-    >
-      <div
-        className={clsx(
-          "fixed inset-0 bg-black/70 transition-all",
-          {
-            "opacity-100 duration-500 ease-in-out visible": openDrawer,
-          },
-          { "opacity-0 duration-500 ease-in-out invisible": !openDrawer },
-        )}
-      ></div>
-      <div className={clsx({ "fixed inset-0": openDrawer })}>
-        <div className="absolute inset-0">
-          <div
-            className={clsx(
-              "pointer-events-none fixed h-5/6",
-              classNames[side],
-            )}
-          >
-            <div
-              className={clsx(
-                "pointer-events-auto relative w-full h-full transform transition ease-in-out duration-500",
-                { [closeClassNames[side]]: !openDrawer },
-                { [openClassNames[side]]: openDrawer },
-              )}
-              onClick={(event) => {
-                event.preventDefault();
-                event.stopPropagation();
-              }}
-            >
-              <div
-                className={clsx(
-                  "flex flex-col h-full overflow-y-scroll p-4 shadow-xl bg-white rounded-t-lg",
-                )}
-              >
-                {!loading ? (
-                  <>
-                    <AiOutlineClose
-                      className="absolute top-5 right-5 text-lg cursor-pointer z-20 text-black/50"
-                      onClick={() => setOpenDrawer(!openDrawer)}
-                    />
-                    <div className="md:px-0 lg:px-20 mb-10">
-                      <UniRow
-                        uni_logo={data.uni_logo}
-                        course_name={data?.course_name}
-                        university={data?.university}
-                        uni_address={data?.uni_address}
-                        country={data?.country}
-                        id={data.id ?? 0}
-                      />
-                      <div className="mt-5">
-                        <CourseSubHeader title="What will I learn?" />
-                        <p className="font-semibold text-textColor text-sm my-4">
-                          Overview of the course
-                        </p>
-                        <p className="text-textColor text-sm my-4 leading-6">
-                          {data.course_description}
-                        </p>
-                        <p className="font-semibold text-textColor text-sm my-5">
-                          Further study
-                        </p>
-                        <p className="text-textColor text-sm my-5 leading-6"></p>
-                        <CourseSubHeader title="Which department am I in?" />
-                        <p className="text-textColor text-sm my-4 leading-6">
-                          School of Nursing and Midwifery
-                        </p>
-                        <CourseSubHeader title="CRICOS" />
-                        <p className="text-textColor text-sm my-4 leading-6">
-                          111902E
-                        </p>
-                        <CourseSubHeader title="Study Options" />
-                        <div className="my-4">
-                          <CoursePriceBox
-                            course_name={data.course_name}
-                            course_fee_additional={data.course_fee_additional}
-                            course_fee={data.course_fee}
-                            currency={data.currency}
-                            uni_address={data.uni_address}
-                            created_at={data.created_at}
-                            id={data.id ?? 0}
-                          />
-                        </div>
-                        <CourseSubHeader title="Entry requirements" />
-                        <p className="font-semibold text-textColor text-sm my-4">
-                          For students from Sri Lanka
-                        </p>
-                        <p className="text-textColor text-sm my-4 leading-6">
-                          {data.entry_requirements}
-                        </p>
-                        <p className="font-semibold text-textColor text-sm my-4">
-                          For international students
-                        </p>
-                        <p className="text-textColor text-sm my-4 leading-6">
-                          IELTS (Academic only) - 7.0 (or better) (no component
-                          lower than 7.0); TOEFL (paper based) - 600 (or better)
-                          (with a minimum Test of Written English Score of TWE
-                          5); TOEFL (electronic or computer based) - 250 (or
-                          better) (with essay rating of at least 5); TOEFL
-                          (internet based) - 95 (or better) (with 22 minimum in
-                          all bands); Pearson (PTE Academic) - 65 (or better)
-                          (with no communicative score less than 65); Cambridge
-                          English Scale Scores for FCE, CAE and CPE - Cambridge
-                          English Advanced (CAE) and Cambridge English
-                          Proficiency (CPE)/ 185 or above. No less than 185 in
-                          each skill.
-                        </p>
-                      </div>
-                      <ApplicationForm intakes={intakes} />
-                    </div>
-                  </>
-                ) : (
-                  <LoadingSpinner />
-                )}
+    <CustomDrawer openDrawer={openDrawer} setOpenDrawer={setOpenDrawer}>
+      {!loading ? (
+        <>
+          <AiOutlineClose
+            className="absolute top-5 right-5 text-lg cursor-pointer z-20 text-black/50"
+            onClick={() => setOpenDrawer(false)}
+          />
+          <div className="md:px-0 lg:px-20 mb-10">
+            <UniRow
+              uni_logo={data.uni_logo}
+              course_name={data?.course_name}
+              university={data?.university}
+              uni_address={data?.uni_address}
+              country={data?.country}
+              id={data.id ?? 0}
+            />
+            <div className="mt-5">
+              <CourseSubHeader title="What will I learn?" />
+              <p className="font-semibold text-textColor text-sm my-4">
+                Overview of the course
+              </p>
+              <p className="text-textColor text-sm my-4 leading-6">
+                {data.course_description}
+              </p>
+              <p className="font-semibold text-textColor text-sm my-5">
+                Further study
+              </p>
+              <p className="text-textColor text-sm my-5 leading-6"></p>
+              <CourseSubHeader title="Which department am I in?" />
+              <p className="text-textColor text-sm my-4 leading-6">
+                School of Nursing and Midwifery
+              </p>
+              <CourseSubHeader title="CRICOS" />
+              <p className="text-textColor text-sm my-4 leading-6">111902E</p>
+              <CourseSubHeader title="Study Options" />
+              <div className="my-4">
+                <CoursePriceBox
+                  course_name={data.course_name}
+                  course_fee_additional={data.course_fee_additional}
+                  course_fee={data.course_fee}
+                  currency={data.currency}
+                  uni_address={data.uni_address}
+                  created_at={data.created_at}
+                  id={data.id ?? 0}
+                />
               </div>
+              <CourseSubHeader title="Entry requirements" />
+              <p className="font-semibold text-textColor text-sm my-4">
+                For students from Sri Lanka
+              </p>
+              <p className="text-textColor text-sm my-4 leading-6">
+                {data.entry_requirements}
+              </p>
+              <p className="font-semibold text-textColor text-sm my-4">
+                For international students
+              </p>
+              <p className="text-textColor text-sm my-4 leading-6">
+                IELTS (Academic only) - 7.0 (or better) (no component lower than
+                7.0); TOEFL (paper based) - 600 (or better) (with a minimum Test
+                of Written English Score of TWE 5); TOEFL (electronic or
+                computer based) - 250 (or better) (with essay rating of at least
+                5); TOEFL (internet based) - 95 (or better) (with 22 minimum in
+                all bands); Pearson (PTE Academic) - 65 (or better) (with no
+                communicative score less than 65); Cambridge English Scale
+                Scores for FCE, CAE and CPE - Cambridge English Advanced (CAE)
+                and Cambridge English Proficiency (CPE)/ 185 or above. No less
+                than 185 in each skill.
+              </p>
             </div>
+            <ApplicationForm intakes={intakes} />
           </div>
+        </>
+      ) : (
+        <div className="mt-12">
+          <LoadingSpinner />
         </div>
-      </div>
-    </div>
+      )}
+    </CustomDrawer>
   );
 };
