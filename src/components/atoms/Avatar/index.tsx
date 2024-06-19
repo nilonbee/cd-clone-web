a82fd780eb2ee71b24d5936de941aa768e475c6b
nilonbee <nilonbee@gmail.com>
2024-06-19 14:27:01 +0530

    fix: use memo hook for utilizing code

--- src/components/atoms/Avatar/index.tsx
+++ src/components/atoms/Avatar/index.tsx
@@ -1,29 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
-'use client';
-import { useUserStore } from '@/store';
-import React from 'react'
-import { ProfileIcon } from '../Icons';
-import { rootImagePath } from '@/utils/rootImagePath';
-
+"use client";
+import { useUserStore } from "@/store";
+import React, { useMemo } from "react";
+import { ProfileIcon } from "../Icons";
+import { rootImagePath } from "@/utils/rootImagePath";
 
 export const Avatar = () => {
-    const { user } = useUserStore();
+  const { user } = useUserStore();
 
-    if (user) {
-        console.log(user.img_url, "user");
+  const imgUrl = useMemo(() => user?.img_url, [user]);
 
-        return (
-            <div>
-                <img
-                    className='w-12 h-12 rounded-full border-none outline-primary object-contain'
-                    src={rootImagePath(user.img_url)}
-                    alt="avatar"
-                />
-            </div>
-        )
-    }
+  if (!imgUrl) {
+    console.log(imgUrl, "user");
 
-    return (
-        <ProfileIcon />
-    )
-}
+    return <ProfileIcon />;
+  }
+
+  return (
+    <div>
+      <img
+        className="w-12 h-12 rounded-full border-none outline-primary object-contain"
+        src={rootImagePath(imgUrl)}
+        alt="avatar"
+      />
+    </div>
+  );
+};
